e848bbb5aa5f1528ac5bf8f2e6b5788b37684b43
hideki.okada <hideki.okada@dena.com>
2020-05-17 16:47:06 +0900

    kanryou

--- site/sugar_water.py
+++ site/sugar_water.py
@@ -3,19 +3,29 @@ A, B, C, D, E, F = list(map(int, input().split()))
 max_concentration = 0
 max_sugar = 0
 max_sugar_water = 0
-for a in range(F // (100 * A) + 1):
-    for b in range((F - 100 * A * a) // (100 * B) + 1):
-        for c in range(E * (a * A + b * B) // C + 1):
+for a in range(31):
+    if a * 100 * A > F:
+        continue
+    for b in range(31):
+        if 100 * A * a + 100 * B * b > F:
+            continue
+        for c in range(3000 // C + 1):
+            if E * (a * A + b * B) < c * C:
+                continue
+            if 100 * A * a + 100 * B * b + c * C > F:
+                continue
+            # 濃度最大化のd
             d = (E * (a * A + b * B) - c * C) // D
-            tmp_sugar = c * C + d * D
-            if tmp_sugar > E * (a * A + b * B):
-                continue
             tmp_sugar_water = a * 100 * A + b * 100 * B + c * C + d * D
-            if tmp_sugar_water > F or tmp_sugar_water == 0:
+            if tmp_sugar_water == 0:
                 continue
+            if tmp_sugar_water > F:
+                # 総量はFまでなのでdを調整
+                d = (F - (a * 100 * A + b * 100 * B + c * C)) // D
+            tmp_sugar_water = a * 100 * A + b * 100 * B + c * C + d * D
+            tmp_sugar = c * C + d * D
             tmp_concentration = tmp_sugar / tmp_sugar_water
-            print(tmp_sugar_water, tmp_sugar, tmp_concentration)
-            if max_concentration < tmp_concentration:
+            if max_concentration <= tmp_concentration:
                 max_concentration = tmp_concentration
                 max_sugar_water = tmp_sugar_water
                 max_sugar = tmp_sugar
